f823869106332548ac1c8e4f4bb7cfce2d6b2828
Anton Vashenko <vashchenko-96@mail.ru>
2022-08-25 09:52:45 +0300

    Add Task 006 and task 007

---
+++ Project006_Sort_Array/Program.cs
@@ -0,0 +1,50 @@
+﻿void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = array[i];
+        int index = i;
+
+        for (int j = 1 + i; j < array.Length; j++)
+        {
+            if(min > array[j]) 
+            {
+                min = array[j];
+                index = j;
+            }
+        }
+
+        int temp = array[index];
+        array[index] = array[i];
+        array[i] = temp;
+    }
+}
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArray(10, 1, 10);
+PrintArray(arr);
+SortArray(arr);
+PrintArray(arr);
---
+++ Project007_Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿void ShowArrayNumber(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i,j] == 1) Console.Write("#");
+            else Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] digit0 =
+{
+    {0,0,1,1,0,0},
+    {0,1,0,0,1,0},
+    {0,1,0,0,1,0},
+    {0,1,0,0,1,0},
+    {0,0,1,1,0,0}
+};
+
+int[,] digit1 =
+{
+    {0,0,0,1,0},
+    {0,0,1,1,0},
+    {0,1,0,1,0},
+    {0,0,0,1,0},
+    {0,0,0,1,0}
+};
+
+Console.Write("Введите число: ");
+char[] num = Console.ReadLine().ToArray();
+
+
+//Console.WriteLine(number)
